f9ce35a5b919ac72297e54f02ad256e67f385e9c
Prashant Jha <jhap5754@gmail.com>
2025-11-14 17:07:22 +0530

    Again minor changes for react-helmet-async

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
 );
